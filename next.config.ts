350e0e2e74db1cde23af4f113fa985aa7ce7c1ee
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-12-18 18:31:06 +0500

    test

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-    output: "standalone",
+  output: "standalone",
+  reactStrictMode: true,
+  metadataBase: new URL("http://softgroup.uz"), 
 };
 
 export default withNextIntl(nextConfig);
