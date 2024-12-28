4996e58fd2ca089071744efc1bc6a306fc2c4f6a
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2024-12-29 04:41:41 +0500

    fix

--- backend/app/core/security/hashing.py
+++ backend/app/core/security/hashing.py
@@ -2,8 +2,8 @@ import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def check_password(password: str, hashed_password: bytes) -> bool:
-    return bcrypt.checkpw(password.encode(), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
