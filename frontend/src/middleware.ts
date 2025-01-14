d416c94e536d8632ad6d0865816c7f53a61523fb
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-14 08:37:32 +0500

    fix i18n

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,14 +3,10 @@ import { NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
 export default async function middleware(request: NextRequest) {
-  const handleI18nRouting = createMiddleware({
-    ...routing,
-    defaultLocale: "uz",
-    localePrefix: "as-needed",
-  });
-
-  return handleI18nRouting(request);
+  const intlMiddleware = createMiddleware(routing);
+  return intlMiddleware(request);
 }
+
 export const config = {
   matcher: ["/", "/(uz|en|ru)/:path*"],
 };
