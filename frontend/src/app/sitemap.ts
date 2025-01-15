5997fc4039146538500c89d591bbbf15855d75ae
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-15 11:11:28 +0500

    fix

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -1,15 +1,15 @@
-import type { MetadataRoute } from "next";
+// import type { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const response = await fetch("https://api.softgroup.uz/templates");
-  const templates = await response.json();
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const response = await fetch("https://api.softgroup.uz/templates");
+//   const templates = await response.json();
 
-  console.log(templates);
+//   console.log(templates);
 
-  return templates?.data.map((template: { slug: string; updated_at: string }) => ({
-    url: `https://templora.uz/templates/${template.slug}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 0.8,
-  }));
-}
+//   return templates?.data.map((template: { slug: string; updated_at: string }) => ({
+//     url: `https://templora.uz/templates/${template.slug}`,
+//     lastModified: new Date(),
+//     changeFrequency: "weekly",
+//     priority: 0.8,
+//   }));
+// }
