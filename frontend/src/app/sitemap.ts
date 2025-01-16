4717a793a3dee6b36b2e61f911c24dea3b1a6f74
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-01-16 11:13:23 +0500

    fix sitemap

---
+++ frontend/src/app/sitemap.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const response = await fetch("https://api.softgroup.uz/templates");
+  const templates = await response.json();
+
+  console.log(templates);
+
+  return templates?.data.map(
+    (template: { slug: string; updated_at: string }) => ({
+      url: `https://templora.uz/templates/${template.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    })
+  );
+}
