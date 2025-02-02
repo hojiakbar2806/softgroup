cdc64a47f3d485095a5308e90772a86f430d88cf
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-02-02 15:36:52 +0500

    fix the dockerfiles

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   images: {
     domains: ["localhost", "127.0.0.1", "templora.uz", "api.softgroup.uz"],
   },
